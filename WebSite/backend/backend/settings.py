448e4227b658e0cdeb96564471a4186817417a5e
Nokotachi <limnotflybyl@gmail.com>
2024-11-03 17:25:05 +0300

    frontend

--- WebSite/backend/backend/settings.py
+++ WebSite/backend/backend/settings.py
@@ -28,9 +28,13 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'WebAndTg',
     'telegramapp',
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
