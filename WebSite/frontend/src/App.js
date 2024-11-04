6cf4d4f1d9c070205717d8769ef2ccad7ad98e7e
Nokotachi <limnotflybyl@gmail.com>
2024-11-04 18:04:16 +0300

    api => frontend

--- WebSite/frontend/src/App.js
+++ WebSite/frontend/src/App.js
@@ -1,33 +1,49 @@
 import Card from "./components/card.js"
+import axios from "axios"
+import React, { Component } from 'react';
+
+const CatalogUrl = "http://127.0.0.1:8000/api/Catalog"
 
 
-export default function App() {
-    return (
-        <div>
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [], // начальное состояние
+        };
+    }
 
-            <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-40 gap-y-10">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+    componentDidMount() {
+        axios.get(CatalogUrl)
+            .then((res) => {
+                this.setState({ data: res.data }); // обновление состояния
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }
 
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+    render() {
+        const { data } = this.state;
 
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+        return (
+            <div>
+                <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-40 gap-y-10">
+                    {data.map((item, index) => (
+                        <Card
+                            key={index}
+                            type={item.Type}
+                            product={item.Product}
+                            photo={item.Photo}
+                            price={item.Price}
+                            description={item.Description}
+                        />
+                    ))}
+                </div>
             </div>
-
-
-
-
-        </div>
-
-    )
+        );
+    }
 }
 
+export default App;
